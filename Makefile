4ebf3323fc550aed51e19e1a17a41e478dcd4eb4
Autsunset <74555644+Autsunset@users.noreply.github.com>
2026-09-14 10:00:37 +0800

    chore(release): prepare v0.10.7

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PLUGIN_ID := cpa-quota-estimator
-VERSION ?= 0.10.6
+VERSION ?= 0.10.7
 GOOS ?= $(shell go env GOOS)
 GOARCH ?= $(shell go env GOARCH)
 EXT := so
--- types.go
+++ types.go
@@ -12,7 +12,7 @@ const (
 	pluginName = "CPA Quota Estimator"
 )
 
-var pluginVersion = "0.10.6"
+var pluginVersion = "0.10.7"
 
 type envelope struct {
 	OK     bool            `json:"ok"`
